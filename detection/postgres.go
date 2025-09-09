20ecbe413e11d324c63d39705047e048c1927cab
RogueProx@mbp.local <dima@MacBook-Pro-Dmitrij.local>
2025-09-09 15:25:39 +0300

    added comment

--- detection/postgres.go
+++ detection/postgres.go
@@ -10,13 +10,16 @@ import (
 
 var pqDialer net.Dialer
 
+// IsPostgresql detects if there is a postgres instance on specified host/port
+//
+// It roughly consumes 1Kb of memory per call
 func IsPostgresql(ctx context.Context, host string, port int) (bool, error) {
-	conn, err := pqDialer.DialContext(ctx, "tcp4", fmt.Sprintf("%s:%d", host, port))
+	conn, err := pqDialer.DialContext(ctx, "tcp4", fmt.Sprintf("%s:%v", host, port))
 	if err != nil {
 		return false, nil
 	}
 	defer conn.Close()
-	inMessage := []byte{
+	inMessage := [...]byte{
 		0x00,
 		0x00,
 		0x00,
@@ -26,7 +29,7 @@ func IsPostgresql(ctx context.Context, host string, port int) (bool, error) {
 		0x00,
 		0x00,
 	}
-	c, err := conn.Write(inMessage)
+	c, err := conn.Write(inMessage[:])
 	if err != nil {
 		return false, nil
 	}
@@ -35,7 +38,7 @@ func IsPostgresql(ctx context.Context, host string, port int) (bool, error) {
 	}
 	var buffer [11]byte
 
-	toRecv := []byte{
+	toRecv := [...]byte{
 		0x45, 0x00, 0x00, 0x00, 0x85, 0x53, 0x46, 0x41, 0x54, 0x41, 0x4c,
 	}
 
@@ -46,7 +49,7 @@ func IsPostgresql(ctx context.Context, host string, port int) (bool, error) {
 	if n != len(toRecv) {
 		return false, nil
 	}
-	if !bytes.Equal(buffer[:n], toRecv) {
+	if !bytes.Equal(buffer[:n], toRecv[:]) {
 		return false, nil
 	}
 	return true, nil
--- detection/test/postgres_test.go
+++ detection/test/postgres_test.go
@@ -52,3 +52,43 @@ func TestDetectPostgres(t *testing.T) {
 	require.False(t, notOk)
 
 }
+
+func BenchmarkTestIsPostgres(b *testing.B) {
+	ctx := context.Background()
+
+	pgContainer, err := postgres.RunContainer(ctx,
+		testcontainers.WithImage("postgres:15.3-alpine"),
+		postgres.WithDatabase("test-db"),
+		postgres.WithUsername("postgres"),
+		postgres.WithPassword("postgres"),
+		testcontainers.WithWaitStrategy(
+			wait.ForLog("database system is ready to accept connections").
+				WithOccurrence(2).WithStartupTimeout(5*time.Second)),
+	)
+	if err != nil {
+		b.Fatal(err)
+	}
+
+	b.Cleanup(func() {
+		if err := pgContainer.Terminate(ctx); err != nil {
+			b.Fatalf("failed to terminate pgContainer: %s", err)
+		}
+	})
+
+	mappedPort, err := pgContainer.MappedPort(ctx, "5432/tcp")
+	require.NoError(b, err)
+
+	mappedPortInt := mappedPort.Int()
+
+	b.ResetTimer()
+	b.ReportAllocs()
+
+	for b.Loop() {
+		_, _ = detection.IsPostgresql(
+			ctx,
+			"localhost",
+			mappedPortInt,
+		)
+	}
+	b.StopTimer()
+}
